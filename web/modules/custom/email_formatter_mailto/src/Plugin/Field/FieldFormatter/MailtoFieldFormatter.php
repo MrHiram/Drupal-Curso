2ea28587d406482e0e2fb6afdb5b9dcf37c07149
mrhiram <mrhiram1@gmail.com>
2020-05-05 11:53:30 -0600

    Mail to formatter module
    Drupal console installed and mailto module created

---
+++ web/modules/custom/email_formatter_mailto/src/Plugin/Field/FieldFormatter/MailtoFieldFormatter.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\email_formatter_mailto\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Plugin implementation of the 'mailto_field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "mailto_field_formatter",
+ *   label = @Translation("Mailto field formatter"),
+ *   field_types = {
+ *     "email"
+ *   }
+ * )
+ */
+class MailtoFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      // Implement settings form.
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // crea la parte de mailto:email@example.com
+    $url = Url::fromUri("mailto:{$item->value}");
+
+    // cree la <a href="$url">Send Mail</a>
+    $link = Link::fromTextAndUrl($this->t("Send Mail"), $url);
+
+    return $link->toString();
+  }
+
+}
